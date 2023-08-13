0fcc0bf4e09514715f98ea0019a42f4f6af174dd
Alex Nguyen <luan.nguyen@brank.as>
2023-08-13 08:31:57 +0700

    update: Missing tests

--- ui/src/pages/Clients/index.test.tsx
+++ ui/src/pages/Clients/index.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import ClientsPage from '.';
 import { StateContext } from '../../store/DataProvider';
+import userEvent from '@testing-library/user-event';
 
 const renderPage = (clients: IClient[] = []) => {
 	const state: IApplicationState = {
@@ -12,6 +13,7 @@ const renderPage = (clients: IClient[] = []) => {
 				email: 'john@gmail.com',
 				phoneNumber: '0123456789',
 			},
+			...clients,
 		],
 	};
 	return render(
@@ -28,5 +30,27 @@ describe('Client Page', () => {
 		expect(await screen.findByTestId('search-input')).toBeInTheDocument();
 		expect(await screen.findByTestId('btn-create')).toBeInTheDocument();
 		expect(await screen.findByTestId('client-table')).toBeInTheDocument();
+		expect(await screen.findAllByTestId('client-row')).toHaveLength(1);
+	});
+
+	test('should display filtered client correct', async () => {
+		renderPage([
+			{
+				firstName: 'Tom',
+				lastName: 'Cruise',
+				email: 'tom@gmail.com',
+				phoneNumber: '0123456789',
+			},
+		]);
+
+		const searchInput = await screen.findByTestId('search-input');
+
+		expect(searchInput).toBeInTheDocument();
+		expect(await screen.findAllByTestId('client-row')).toHaveLength(2);
+
+		userEvent.type(searchInput, 'Tom');
+		await waitFor(async () => {
+			expect(await screen.findAllByTestId('client-row')).toHaveLength(1);
+		});
 	});
 });
